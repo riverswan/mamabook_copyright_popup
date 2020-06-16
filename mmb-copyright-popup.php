12f756b958abc470e89ef81634a0a23cebda09bc
Pavel Lebedev <maxwinrate@gmail.com>
2020-06-16 22:02:48 +0300

    Init

---
+++ mmb-copyright-popup.php
@@ -0,0 +1,19 @@
+<?php
+
+
+/**
+ * Plugin Name:       Mmb Copyright Popup
+ * Plugin URI:        https://github.com/riverswan/penci_slider_style_28_fix
+ * Description:       Quick fix for Style №28 homepage slider of Soledad WordPress theme.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Paul Swan
+ * Author URI:        https://github.com/riverswan
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       penci-28-fix
+ * Domain Path:       /languages
+ */
+
+
