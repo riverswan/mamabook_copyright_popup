99ee58e6d81fc1424f3a3cf472ce3318be82b90b
Pavel Lebedev <maxwinrate@gmail.com>
2020-06-16 22:44:09 +0300

    Added alert popup

--- mmb-copyright-popup.js
+++ mmb-copyright-popup.js
@@ -1,1 +1,10 @@
-alert('HI')
+(function () {
+    document.body.addEventListener('copy', (event) => {
+        const selection = document.getSelection();
+        const alertMessage = 'Копіювання матеріалів з сайту дозволено лише за умови вказаного гіперлінку на наш сайт. А копіювання матеріалів з метою розміщення на vseosvita.ua категорично заборонено і призведе до судового позову.'
+        const addedText = '  Копіювання матеріалів з сайту <a>https://mamabook.com.ua</a> дозволено лише за умови вказаного гіперлінку на наш сайт. А копіювання матеріалів з метою розміщення на vseosvita.ua категорично заборонено і призведе до судового позову.'
+        alert(alertMessage)
+        event.clipboardData.setData('text/plain', selection.toString() + addedText);
+        event.preventDefault();
+    });
+})();
