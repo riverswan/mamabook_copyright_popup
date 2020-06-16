5b2fd8aa02134a9ff50ff4cd87d719904183bf66
Pavel Lebedev <maxwinrate@gmail.com>
2020-06-16 23:22:54 +0300

    Localstorage date

--- mmb-copyright-popup.js
+++ mmb-copyright-popup.js
@@ -3,8 +3,22 @@
         const selection = document.getSelection();
         const alertMessage = 'Копіювання матеріалів з сайту дозволено лише за умови вказаного гіперлінку на наш сайт. А копіювання матеріалів з метою розміщення на vseosvita.ua категорично заборонено і призведе до судового позову.'
         const addedText = '  Копіювання матеріалів з сайту <a>https://mamabook.com.ua</a> дозволено лише за умови вказаного гіперлінку на наш сайт. А копіювання матеріалів з метою розміщення на vseosvita.ua категорично заборонено і призведе до судового позову.'
-        alert(alertMessage)
+        let now = new Date();
+        let year = now.getFullYear();
+        let month = now.getMonth() + 1;
+        let date = now.getDate();
+        if (!localStorage.getItem('mamabook_alert_message_date')) {
+            alert(alertMessage);
+            localStorage.setItem('mamabook_alert_message_date', `${year}/${month}/${date}`)
+        } else {
+            if (localStorage.getItem('mamabook_alert_message_date') < `${year}/${month}/${date}` ){
+                alert(alertMessage);
+                localStorage.setItem('mamabook_alert_message_date', `${year}/${month}/${date}`)
+            }
+        }
         event.clipboardData.setData('text/plain', selection.toString() + addedText);
         event.preventDefault();
     });
+
+
 })();
